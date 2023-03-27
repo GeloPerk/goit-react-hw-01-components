9243b08637b668414a7b9fb29436181e14d40f5f
GeloPerk <114014987+GeloPerk@users.noreply.github.com>
2023-03-27 17:09:35 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import { Profile } from './Profile/Profile';
+import user from '../user.json';
+import { Statistics } from './data/data';
+import statistics from '../data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        padding: 50,
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 24,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" statistics={statistics} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
